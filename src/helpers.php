b6677e1ae4af561a296b47f5fed52636a3ea7ebd
Omar Bero <omarpero85@gmail.com>
2023-12-13 23:47:31 +0200

    add-getCurrentDeviceSignature-helper

--- src/helpers.php
+++ src/helpers.php
@@ -9,3 +9,16 @@ if (!function_exists('getCurrentDeviceSignature')) {
             request()->headers->get('X-DEVICE-SIGNATURE');
     }
 }
+
+if (!function_exists('getCurrentDeviceSignature')) {
+    function getCurrentDeviceSignature()
+    {
+        $signature = trim(
+            session()->isStarted() ?
+                session()->get('X-DEVICE-SIGNATURE') :
+                request()->headers->get('X-DEVICE-SIGNATURE'),
+            "\s"
+        );
+        return !empty($signature) ? $signature : null;
+    }
+}
