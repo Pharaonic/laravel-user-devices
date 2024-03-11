090c4004cc28e6c7a73d00bd708eeae7566180ac
Moamen Eltouny <16774083+MoamenEltouny@users.noreply.github.com>
2024-03-11 15:45:29 +0200

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -4,10 +4,8 @@ if (!function_exists('getCurrentDeviceSignature')) {
     function getCurrentDeviceSignature()
     {
         $signature = trim(
-            session()->isStarted() ?
-                session()->get('X-DEVICE-SIGNATURE') :
-                request()->headers->get('X-DEVICE-SIGNATURE'),
-            ' '
+            session()->isStarted() ? session()->get('X-DEVICE-SIGNATURE', '') : request()->headers->get('X-DEVICE-SIGNATURE', ''),
+            "\s"
         );
 
         return !empty($signature) ? $signature : null;
